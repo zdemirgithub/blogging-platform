617451f0cecf8d80198f02c2162d49a6c1274634
Zehra Demir <zehrademir.gm@gmail.com>
2024-12-15 18:19:27 +0300

    Create User.js

---
+++ backend/models/User.js
@@ -0,0 +1,26 @@
+// backend/models/User.js
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+UserSchema.methods.comparePassword = function (password) {
+  return bcrypt.compareSync(password, this.password);
+};
+
+module.exports = mongoose.model("User", UserSchema);
